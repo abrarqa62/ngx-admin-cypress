2420ae35457e3986cd7c8c31ffbf4db83f1848de
abrarqa62 <abrar.razzaq@devsinc.com>
2022-06-14 20:50:46 +0500

    Full Automate E-commerce

--- cypress/integration/EcommerceAutomation.spec.js
+++ cypress/integration/EcommerceAutomation.spec.js
@@ -1,4 +1,3 @@
-import { equal } from "assert";
 import { NavigateTo } from "../support/page_objects/PageObjectsEcommerce";
 
 describe("Ecommerce Page", () => {
@@ -6,22 +5,24 @@ describe("Ecommerce Page", () => {
     cy.visit("/");
   });
 
-  //    Profit card
-  context("When user move on profit card", () => {
-    it("User is on Profit Card Section", () => {
+  //  Profit card
+
+  context("When user is on profit card", () => {
+
+    it("Verify if user is on Profit Card Section", () => {
       NavigateTo.ProfitCardNavigation();
     });
-    it("User Click on FlipIcon ", () => {
+    it("Verify if user Click on FlipIcon ", () => {
       NavigateTo.ProfitCardFlipIcon().click();
     });
-    it("User Click on FlipIcon to go back ", () => {
+    it("Verify if user Click on FlipIcon to go back ", () => {
       NavigateTo.ProfitbackFlipicon().click();
     });
-    it("Test the Chart functionality by mouse hover on it", () => {
+    it("Verify if user mouse hover on the Front side profit Chart", () => {
       NavigateTo.PofitCardChart().then(($canvas) => {
         let c = cy.wrap($canvas);
         let WidthCanvas = $canvas.width();
-        let HeightCanvas = $canvas.height();xdescribe
+        let HeightCanvas = $canvas.height();
 
         let pointXaxis1 = WidthCanvas * 0.71;
         let PointYaxis1 = HeightCanvas * 0.63;
@@ -30,24 +31,22 @@ describe("Ecommerce Page", () => {
         let pointXaxis3 = WidthCanvas * 0.92;
         let PointYaxis3 = HeightCanvas * 0.62;
 
-        c.click(pointXaxis1, PointYaxis1).wait(300);
-
-        c.click(pointXaxis2, PointYaxis2).wait(300);
-
-        c.click(pointXaxis3, PointYaxis3).wait(300);
+        c.click(pointXaxis1, PointYaxis1);
+        c.click(pointXaxis2, PointYaxis2);
+        c.click(pointXaxis3, PointYaxis3);
       });
     });
 
     it("Click on the flipicon to test the backgrapgh", () => {
       NavigateTo.ProfitCardFlipIcon().click({ force: true });
     });
-    it("Test the Backside graph ", () => {
+    it("Verify if user mouse hover on the Back  side profit Chart", () => {
       NavigateTo.ProfitcardBackGraph().then(($BackCanvas) => {
         let v = cy.wrap($BackCanvas);
 
-        v.click(5, 130).wait(150);
-        v.click(50, 110).wait(150);
-        v.click(90, 120).wait(150);
+        v.click(5, 130);
+        v.click(50, 110);
+        v.click(90, 120);
       });
     });
     it("Now navigate back to Profit card front", () => {
@@ -59,149 +58,355 @@ describe("Ecommerce Page", () => {
   // Earning Card
 
   context("When user now move to Earning Card", () => {
-    it("Test the Daily Income Chart", () => {
+
+    it("Verify if user mouse hover on the Daily Income Chart", () => {
       NavigateTo.EarningChart().then(($Earningcanvas) => {
         let e = cy.wrap($Earningcanvas);
         cy.log($Earningcanvas.width());
         cy.log($Earningcanvas.height());
 
-        e.click(10, 130).wait(300);
-        e.click(40, 110).wait(300);
-        e.click(75, 120).wait(300);
-        e.click(105, 130).wait(300);
-
+        e.click(10, 130);
+        e.click(40, 110);
+        e.click(105, 130);
       });
     });
 
-      it('User now select Tether from Dropdown', () => {
-        NavigateTo.DropdownButtonEarning().click().should('be.visible');
-        NavigateTo.DropdownListEarning().contains('Tether').click();
+    it("Verify if user now select Tether from Dropdown", () => {
+      NavigateTo.DropdownButtonEarning().should("be.visible").click();
+      NavigateTo.DropdownListEarning().contains("Tether").click();
 
-        NavigateTo.EarningChart().then(($Earningcanvas) => {
-          let e = cy.wrap($Earningcanvas);
-          cy.log($Earningcanvas.width());
-          cy.log($Earningcanvas.height());
+      NavigateTo.EarningChart().then(($Earningcanvas) => {
+        let e = cy.wrap($Earningcanvas);
+        cy.log($Earningcanvas.width());
+        cy.log($Earningcanvas.height());
 
-          e.click(10, 130).wait(300);
-          e.click(50, 110).wait(300);
-          e.click(90, 120).wait(300);
-          e.click(105, 130).wait(300);
-
-        });
-
+        e.click(10, 130);
+        e.click(50, 110);
+        e.click(90, 120);
+        e.click(105, 130);
       });
-      it('User now select Ehtereum from Dropdown', () => {
-        NavigateTo.DropdownButtonEarning().click().should('be.visible');
-        NavigateTo.DropdownListEarning().contains('Ethereum').click();
-
-        NavigateTo.EarningChart().then(($Earningcanvas) => {
-          let e = cy.wrap($Earningcanvas);
-          cy.log($Earningcanvas.width());
-          cy.log($Earningcanvas.height());
-
-          e.click(10, 130).wait(300);
-          e.click(50, 110).wait(300);
-          e.click(90, 120).wait(300);
-          e.click(105, 130).wait(300);
-
-        });
-
-      });
-      it('User click on flipicon to navigate Earning Card backside', () => {
-        NavigateTo.EarningcardflipIcon().click();
-        NavigateTo.EarningCoinNameback().contains('Bitcoin');
-      });
-      
-    it('User click Green Color in PieChart', () => {
-      NavigateTo.EarningPieChart().click(82,112).wait(550);
-      NavigateTo.EarningCoinNameback().should('contain','Bitcoin');
     });
-    it('User click Orange Color in PieChart', () => {
-      NavigateTo.EarningPieChart().click(28,38).wait(550);
-      NavigateTo.EarningCoinNameback().should('contain','Ethereum');
+    it("Verify if user now select Ehtereum from Dropdown", () => {
+      NavigateTo.DropdownButtonEarning().click().should("be.visible");
+      NavigateTo.DropdownListEarning().contains("Ethereum").click();
+
+      NavigateTo.EarningChart().then(($Earningcanvas) => {
+        let e = cy.wrap($Earningcanvas);
+        cy.log($Earningcanvas.width());
+        cy.log($Earningcanvas.height());
+
+        e.click(10, 130);
+        e.click(50, 110);
+        e.click(90, 120);
+        e.click(105, 130);
+      });
     });
-    it('User click Blue Color in PieChart', () => {
-      NavigateTo.EarningPieChart().click(40,105).wait(550);
-      NavigateTo.EarningCoinNameback().should('contain','Tether');
+    it("Verify if user click on flipicon to navigate Earning Card backside", () => {
+      NavigateTo.EarningcardflipIcon().click();
+      NavigateTo.EarningCoinNameback().contains("Bitcoin");
+    });
+
+    it("Verify if user click Green Color in PieChart", () => {
+      NavigateTo.EarningPieChart().click(82, 112);
+      NavigateTo.EarningCoinNameback().should("contain", "Bitcoin");
+    });
+    it("Verify if user click Orange Color in PieChart", () => {
+      NavigateTo.EarningPieChart().click(28, 38);
+      NavigateTo.EarningCoinNameback().should("contain", "Ethereum");
+    });
+    it("Verify if user click Blue Color in PieChart", () => {
+      NavigateTo.EarningPieChart().click(40, 105);
+      NavigateTo.EarningCoinNameback().should("contain", "Tether");
     });
   });
 
   // Traffic Card Front Side
 
   context("When user now move to Traffic Card", () => {
-    it("User is now at Traffic Card", () => {
+    
+    it("Verify if user is now at Traffic Card", () => {
       NavigateTo.TrafficCardFront().should("contain", "Mon");
     });
 
-    it("User click on Traffic Dropdown", () => {
+    it("Verify if user click on Traffic Dropdown", () => {
       NavigateTo.TrafficFrontDropdownButton().eq(0).click();
     });
 
-    it("User select Month from Dropdown list", () => {
+    it("Verify if  user select Month from Dropdown list", () => {
       NavigateTo.FrontTrafficDropdownList().contains("month").click();
       NavigateTo.TrafficCardFront().should("contain", "Feb");
     });
 
-    it("User click on Traffic Dropdown", () => {
+    it("Verify if user click on Traffic Dropdown", () => {
       NavigateTo.TrafficFrontDropdownButton().eq(0).click();
     });
 
-    it("User select Year from Dropdown list", () => {
+    it("Verify if user select Year from Dropdown list", () => {
       NavigateTo.FrontTrafficDropdownList().contains("year").click();
       NavigateTo.TrafficCardFront().should("contain", "2011");
     });
-    it("User click on Traffic Dropdown", () => {
+    it("Verify if user click on Traffic Dropdown", () => {
       NavigateTo.TrafficFrontDropdownButton().eq(0).click();
     });
-    it("User select Year from Dropdown list", () => {
+    it("Verify if user select Year from Dropdown list", () => {
       NavigateTo.FrontTrafficDropdownList().contains("week").click();
     });
 
-    // Traffic Card Back Side
- 
-    it('Verify that the user can click on chevron up to see Traffic Bar Chart', () => {
+  // Traffic Card Back Side
+
+    it("Verify that the Verify if user can click on chevron up to see Traffic Bar Chart", () => {
       NavigateTo.TrafficFlipIcon().click();
     });
-    it('User mouse hover on Weekly Traffic Chart ',() => {
+    it("Verify if user mouse hover on Weekly Traffic Chart ", () => {
       NavigateTo.TrafficBarChart()
-      .click(460,210).wait(200)
-      .click(300,180).wait(200)
-      .click(220,150).wait(200)
-      .click(130,140).wait(200)
-      .click(50,160).wait(200);
-      });
-    it('User again click on dropdown',() => {
+        .click(460, 210)
+        .click(300, 180)
+        .click(220, 150)
+        .click(130, 140)
+        .click(50, 160);
+    });
+    it("Verify if user again click on dropdown", () => {
       NavigateTo.TrafficFrontDropdownButton().eq(1).click();
     });
 
-    it('User select Month from dropdown list',() => {
-      NavigateTo.BackTrafficDropdownList().contains('month').click();
+    it("Verify if user select Month from dropdown list", () => {
+      NavigateTo.BackTrafficDropdownList().contains("month").click();
     });
-    it('User mouse hover on Monthly Traffic Chart ',() => {
+    it("Verify if user mouse hover on Monthly Traffic Chart ", () => {
       NavigateTo.TrafficBarChart()
-      .click(420,180).wait(200) 
-      .click(320,140).wait(200)
-      .click(220,180).wait(200)
-      .click(120,150).wait(200);
-      });
-      it('User again click on dropdown',() => {
-        NavigateTo.TrafficFrontDropdownButton().eq(1).click();
-      });
-  
-      it('User select Year from dropdown list',() => {
-        NavigateTo.BackTrafficYearDropdownList().contains('year').click();
-      });
-      it('User mouse hover on Monthly Traffic Chart ',() => {
-        NavigateTo.TrafficBarChart()
-        .click(420,180).wait(200) 
-        .click(300,140).wait(200)
-        .click(220,180).wait(200)
-        .click(110,130).wait(200);
-        
+        .click(420, 180)
+        .click(320, 140)
+        .click(220, 180)
+        .click(120, 150);
     });
-    it('User click on down arrow to move back to Front side of Traffic Card', () => {
-      NavigateTo.TrafficCardBack().click();
-    });
+    it("Verify if user again click on dropdown", () => {
+      NavigateTo.TrafficFrontDropdownButton().eq(1).click();
     });
 
+    it("Verify if user select Year from dropdown list", () => {
+      NavigateTo.BackTrafficYearDropdownList().contains("year").click();
+    });
+    it("Verify if user mouse hover on Monthly Traffic Chart ", () => {
+      NavigateTo.TrafficBarChart()
+        .click(420, 180)
+        .click(300, 140)
+        .click(220, 180)
+        .click(110, 130);
+    });
+    it("Verify if user click on down arrow to move back to Front side of Traffic Card", () => {
+      NavigateTo.TrafficCardBack().click();
+    });
+  });
+
+  // /// MarketPlace Orders Card
+
+  context("When user move to MarketPlace Card", () => {
+    it("Verify if user is on Orders Tab In MarketPlace Card", () => {
+      NavigateTo.MarketplaceEccommerce();
+    });
+
+    it("Verify if user can mouse hover on Grapgh to check Weekly data of Orders", () => {
+      NavigateTo.MarketplaceEccommerce();
+      NavigateTo.OrdersMarketplaceGraph()
+        .click(850, 90)
+        .click(850, 130)
+        .click(540, 220)
+        .click(420, 180)
+        .click(285, 150)
+        .click(240, 180)
+        .click(120, 130);
+    });
+    it("Verify if user click on Orders Dropdown button", () => {
+      NavigateTo.MarketplaceEccommerce();
+      NavigateTo.MarketplaceDropdown().eq(0).click();
+    });
+    it("Verify if user select Month from Orders Dropdown List", () => {
+      NavigateTo.DropdownListOrders().contains("month").click();
+    });
+    it("Verify if user can mouse hover on Graph to check Monthly data of Orders", () => {
+      NavigateTo.MarketplaceEccommerce();
+      NavigateTo.OrdersMarketplaceGraph()
+        .click(759, 192)
+        .click(759, 123)
+        .click(495, 180)
+        .click(377, 212)
+        .click(330, 210)
+        .click(160, 125);
+    });
+    it("Verify if user click on Orders Dropdown button", () => {
+      NavigateTo.MarketplaceEccommerce();
+      NavigateTo.MarketplaceDropdown().eq(0).click();
+    });
+    it("Verify if user select Year from Orders Dropdown List", () => {
+      NavigateTo.DropdownListOrders().contains("year").click();
+    });
+    it("Verify if user can mouse hover on Graph to check Yearly data of Orders", () => {
+      NavigateTo.MarketplaceEccommerce();
+      NavigateTo.OrdersMarketplaceGraph()
+        .click(175, 25)
+        .click(300, 70)
+        .click(420, 135)
+        .click(725, 264);
+    });
+    it("Verify if user click on Orders Dropdown button", () => {
+      NavigateTo.MarketplaceEccommerce();
+      NavigateTo.MarketplaceDropdown().eq(0).click();
+    });
+    it("Verify if user select Year from Orders Dropdown List", () => {
+      NavigateTo.DropdownListOrders().contains("week").click();
+    });
+
+  //   //MarketPlace Profit Card
+
+    it("Verify if user now click on Profits tab to see Marketplace Profit Data", () => {
+      NavigateTo.MarketplaceCardTabs().contains("Profit").click();
+    });
+    it("Verify if user can mouse hover on Graph to check Weekly data of Profit", () => {
+      NavigateTo.ProfitMarketPlaceGraph()
+        .click(850, 90)
+        .click(850, 130)
+        .click(540, 220)
+        .click(420, 180)
+        .click(285, 150)
+        .click(240, 180)
+        .click(120, 130);
+    });
+    it("Verify if user click on Profit Dropdown button", () => {
+      NavigateTo.MarketplaceDropdown().eq(1).click();
+    });
+    it("Verify if user select Month from Profit Dropdown List", () => {
+      NavigateTo.DropdownListProfitMonthly().contains("month").click();
+    });
+    it("Verify if user can mouse hover on Graph to check Monthly data of Profit", () => {
+      NavigateTo.ProfitMarketPlaceGraph()
+        .click(759, 192)
+        .click(759, 123)
+        .click(495, 180)
+        .click(377, 212)
+        .click(330, 210)
+        .click(160, 125);
+    });
+    it("Verify if user click on Profit Dropdown button", () => {
+      NavigateTo.MarketplaceDropdown().eq(1).click();
+    });
+    it("Verify if user select Month from Profit Dropdown List", () => {
+      NavigateTo.DropdownListProfitYearly().contains("year").click();
+    });
+    it("Verify if user can mouse hover on Graph to check Yearly data of Profit", () => {
+      NavigateTo.ProfitMarketPlaceGraph()
+        .click(850, 90)
+        .click(850, 130)
+        .click(540, 220)
+        .click(420, 180)
+        .click(285, 150)
+        .click(240, 180)
+        .click(120, 130);
+    });
+    it("Verify if user navigate back to the Profit Tab", () => {
+      NavigateTo.MarketplaceCardTabs().contains("Orders").click();
+    });
+  });
+  // Visitors Analytics Card
+  context("When user move to Visitors Analytics Card", () => {
+    it("Verify if user is on Visitors Analytics Card ", () => {
+      NavigateTo.VistorAnalyticsCard().should("contain", "Visitors Analytics");
+    });
+
+    it("Verify if user click on Forward arrow to hide Visitors Statistics", () => {
+      NavigateTo.VistorsGraph();
+      NavigateTo.showHideToggle().scrollIntoView().should("be.visible").click();
+    });
+    it("Verify if user mouse hover on Visitor Analytics Chart", () => {
+      NavigateTo.VistorAnalyticsCard();
+      NavigateTo.VistorsGraph()
+        .click(1020, 172)
+        .click(940, 188)
+        .click(825, 190)
+        .click(730, 183)
+        .click(610, 180)
+        .click(525, 170)
+        .click(440, 173)
+        .click(310, 168)
+        .click(220, 172)
+        .click(118, 183);
+    });
+    it("Verify if user again click on Back arrow to show Visitors Statistics", () => {
+      NavigateTo.VistorsGraph();
+      NavigateTo.showHideToggle().scrollIntoView().should("be.visible").click();
+    });
+  });
+
+  // //Country Orders Statistics
+  context("When user move to Country Orders Statistics", () => {
+    it("Verify if User is on the Country Orders Statistics to see the orders stats", () => {
+      NavigateTo.CountryStatsCard().scrollIntoView().should("be.visible");
+    });
+
+    it("Verify if user click on  Venezuela Country ", () => {
+      NavigateTo.CountryStatsGraph().click(320, 300);
+      NavigateTo.CounrtyName().should("contain", "Venezuela");
+    });
+
+    it("Verify user click on Colombia country ", () => {
+      NavigateTo.CountryStatsGraph().click(300, 310);
+      NavigateTo.CounrtyName().should("contain", "Colombia");
+    });
+
+    it("Verify if user click on  Canada country ", () => {
+      NavigateTo.CountryStatsGraph().click(250, 90);
+      NavigateTo.CounrtyName().should("contain", "Canada");
+    });
+
+    it("Verify if user click on  Greenland country ", () => {
+      NavigateTo.CountryStatsGraph().click(360, 30);
+      NavigateTo.CounrtyName().should("contain", "Greenland");
+    });
+
+    // Progress Card
+    context("When user move to Progress Section", () => {
+      it("Verify if user see the Todays Profit on  Progress card ", () => {
+        NavigateTo.ProgressCard().should("contain", "Today’s Profit");
+      });
+
+      it("Verify if user see the New Orders  on  Progress card ", () => {
+        NavigateTo.ProgressCard().should("contain", "New Orders");
+      });
+
+      it("Verify if user see the New Comments on  Progress card ", () => {
+        NavigateTo.ProgressCard().should("contain", "New Comments");
+      });
+    });
+  //  Progress Card
+    context("When user move to UserActivity Section", () => {
+      it("Verify if user is on User Activity card ", () => {
+        NavigateTo.UserActivityCard()
+          .scrollIntoView()
+          .should("contain", "User Activity");
+      });
+      it("Verify if user click on Orders Dropdown button", () => {
+        NavigateTo.UserActivityCard();
+        NavigateTo.UserActivityDropdown().click();
+      });
+      it("Verify if user select Week from dropdown list to see Weekly User Activity ", () => {
+        NavigateTo.UserListDropdown().contains("week").click();
+        NavigateTo.UserActivityCard().should("contain", "Tue");
+      });
+      it("Verify if user click on Orders Dropdown button", () => {
+        NavigateTo.UserActivityCard();
+        NavigateTo.UserActivityDropdown().click();
+      });
+      it("Verify if user select Year from dropdown list to see Yearly User Activity ", () => {
+        NavigateTo.UserListDropdown().contains("year").click();
+        NavigateTo.UserActivityCard().should("contain", "2010");
+      });
+      it("Verify if user click on Orders Dropdown button", () => {
+        NavigateTo.UserActivityCard();
+        NavigateTo.UserActivityDropdown().click();
+      });
+      it("Verify if user select Month from dropdown list to see Monthly User Activity ", () => {
+        NavigateTo.UserListDropdown().contains("month").click();
+        NavigateTo.UserActivityCard().should("contain", "Jun");
+      });
+    });
+  });
 });
--- cypress/support/page_objects/PageObjectsEcommerce.js
+++ cypress/support/page_objects/PageObjectsEcommerce.js
@@ -1,6 +1,5 @@
-import { get } from "core-js/core/dict";
-
 export class Ecommerce {
+
   ProfitCardNavigation() {
     return cy.get('[ data-test-id="ProfitCardfront"]');
   }
@@ -19,7 +18,7 @@ export class Ecommerce {
   ProfitcardBackGraph() {
     return cy.get('[data-test-id="ProfitBackGraph"]');
   }
-  // Earning Card
+  // Earning Section 
 
   EarningChart() {
     return cy.get('[data-test-id="EarningChart"]');
@@ -28,7 +27,7 @@ export class Ecommerce {
     return cy.get('[data-test-id="EarningDropdownButton"]');
   }
   DropdownListEarning() {
-    return cy.get(".option-list");
+    return cy.get('[data-test-id="ListEarningDropdown"]');
   }
   EarningcardflipIcon() {
     return cy.get('[data-test-id="EarningCardback"]');
@@ -41,7 +40,7 @@ export class Ecommerce {
     return cy.get('[data-test-id="PieChartCoins"]');
   }
 
-  /// Traffic Card
+  /// Traffic Section 
 
   TrafficFrontDropdownButton() {
     return cy.get('[data-test-id="FrontTrafficdropdown"]');
@@ -60,14 +59,85 @@ export class Ecommerce {
     return cy.get('[data-test-id="TrafficBarChart"]');
   }
   BackTrafficDropdownList() {
-    return cy.get('#nb-option-7');
+    return cy.get("#nb-option-7");
   }
   BackTrafficYearDropdownList() {
-    return cy.get('#nb-option-8');
+    return cy.get("#nb-option-8");
   }
   TrafficCardBack() {
     return cy.get('[data-test-id="TrafficFrontSide"]');
   }
-}
 
+  // MarketPlace  Section
+
+  MarketplaceEccommerce() {
+    return cy.get('[data-test-id="MarketpalceCard"]');
+  }
+  OrdersMarketplaceGraph() {
+    return cy.get('[data-test-id="OrdersGraph"]');
+  }
+  MarketplaceDropdown() {
+    return cy.get('[data-test-id="Marketdropdown"]');
+  }
+  DropdownListOrders() {
+    return cy.get('[data-test-id="ListMarketdropdown"]');
+  }
+  
+  MarketplaceCardTabs() {
+    return cy.get(".tabset");
+  }
+  ProfitMarketPlaceGraph() {
+    return cy.get('[data-test-id="ProfitGraph"]');
+  }
+  DropdownListProfitWeekly() {
+    return cy.get('[data-test-id="ListMarketdropdown"]');
+  }
+  
+  DropdownListProfitMonthly() {
+    return cy.get('[data-test-id="ListMarketdropdown"]');
+  }
+  DropdownListProfitYearly() {
+    return cy.get('[data-test-id="ListMarketdropdown"]');
+  }
+  //Visitor Analytics Section
+
+  VistorAnalyticsCard() {
+    return cy.get('[data-test-id="VisitorCard"]');
+  }
+  VistorsGraph() {
+    return cy.get('[data-test-id="VisitorsChart"]');
+  }
+  showHideToggle() {
+    return cy.get('[data-test-id="Show&HideToggle"]');
+  }
+  // Country Orders Statistics Section
+  CountryStatsCard() {
+    return cy.get('[data-test-id="CountryCard"]');
+  }
+  CounrtyName(){
+    return cy.xpath('//nb-card-body//ngx-country-orders-chart');
+  }
+  CountryStatsGraph(){
+    return cy.get('[class="leaflet-container leaflet-touch leaflet-fade-anim leaflet-grab leaflet-touch-drag leaflet-touch-zoom"]');
+  }
+
+  // Progress Section
+
+  ProgressCard(){
+    return cy.get('[data-test-id="ProgressView"]');
+  }
+
+  // User Activity Section
+
+  UserActivityCard(){
+    return cy.get('[data-test-id="Useractivity"]');
+  }
+  UserActivityDropdown(){
+    return cy.get('[data-test-id="UserDropdown"]');
+  }
+ UserListDropdown() {
+    return cy.get('[data-test-id="ListDropdown"]');
+  }
+ 
+}
 export const NavigateTo = new Ecommerce();
